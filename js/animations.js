5df114d49b1f4909e72a17beff63abebd5df3e43
EidenVI <michaelpiasetskyi@gmail.com>
2025-03-25 01:02:35 +0300

    339 changes...

---
+++ js/animations.js
@@ -0,0 +1,15 @@
+var linkedinAnimation = lottie.loadAnimation({
+    container: document.getElementById('linkedin-animation'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/icons/dynamic/Linkedin/linkedin.json'
+});
+
+var mailAnimation = lottie.loadAnimation({
+    container: document.getElementById('mail-animation'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    path: 'assets/icons/dynamic/Mail/mail.json'
+});
