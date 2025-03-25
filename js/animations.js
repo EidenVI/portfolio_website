89d48dc68cb1ca4119a5fadc61be108308da02cc
EidenVI <michaelpiasetskyi@gmail.com>
2025-03-26 01:39:24 +0300

    Add message_block

--- js/animations.js
+++ js/animations.js
@@ -13,3 +13,36 @@ var mailAnimation = lottie.loadAnimation({
     autoplay: false,
     path: 'assets/icons/dynamic/Mail/mail.json'
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+})
+
+const observer_blocks = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+}, {
+    threshold: 0.2
+})
+
+const hiddenElements = document.querySelectorAll('.first-layer');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const fadeInElements = document.querySelectorAll('.second-layer, .nah-layer, .third-layer');
+fadeInElements.forEach((el) => observer.observe(el));
+
+const messageBlock = document.querySelector('.message-block');
+observer_blocks.observe(messageBlock);
